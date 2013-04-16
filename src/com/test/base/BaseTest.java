8260e12a980430d1116078a595ef3dc79683029d
huozhicheng <huozhicheng@gmail.com>
2013-04-16 11:45:23 +0800

    测试类更新

--- src/com/test/base/BaseTest.java
+++ src/com/test/base/BaseTest.java
@@ -13,6 +13,11 @@ import org.junit.Test;
 public class BaseTest {
 	@Test
 	public void strTest(){
-		System.out.println("1.0".compareTo("1.3"));
+		//System.out.println("1.0".compareTo("1.3"));
+		BaseTest obj = new BaseTest();
+		System.out.println(obj.getClass().getSimpleName());
+		System.out.println(obj.getClass().getSimpleName());
+		System.out.println(BaseTest.class.getSimpleName());
+		
 	}
 }
